b3802a43ee8032d98261d82f71199cc0d04292a9
cambierr <romain@shareif.com>
2016-08-04 09:18:58 +0200

    fixed null pointer by adding the sha encoder as static field

--- src/main/java/com/github/lynxdb/server/run/Setup.java
+++ src/main/java/com/github/lynxdb/server/run/Setup.java
@@ -32,80 +32,77 @@ import com.github.lynxdb.server.core.Vhost;
 import org.springframework.cassandra.core.keyspace.CreateKeyspaceSpecification;
 import org.springframework.cassandra.core.keyspace.CreateTableSpecification;
 import org.springframework.data.cassandra.core.CassandraTemplate;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 
 /**
  *
  * @author honorem
  */
-
 public class Setup {
-    
+
     private static CassandraTemplate ct;
-    
+
     public static void main(String[] args) {
-        
+
         init();
-        
+
         Vhost system = Vhost.getSystemVhost();
-        
+
         CreateKeyspaceSpecification keyspaceSpec = new CreateKeyspaceSpecification(System.getenv("cassandra_keyspace"))
                 .ifNotExists()
                 .withSimpleReplication(1);
-        
+
         ct.execute(keyspaceSpec);
-        
+
         init(System.getenv("cassandra_keyspace"));
-        
+
         CreateTableSpecification vhosts = CreateTableSpecification.createTable("vhosts")
                 .ifNotExists()
                 .partitionKeyColumn("vhostid", DataType.uuid())
                 .column("vhostname", DataType.text());
-        
+
         ct.execute(vhosts);
-        
-        
+
         CreateTableSpecification users = CreateTableSpecification.createTable("users")
                 .ifNotExists()
                 .partitionKeyColumn("userlogin", DataType.text())
                 .column("userpassword", DataType.text())
                 .column("rank", DataType.text())
                 .column("vhostid", DataType.uuid());
-        
+
         ct.execute(users);
-        
+
         ct.getSession().execute("CREATE MATERIALIZED VIEW IF NOT EXISTS lynx.users_by_vhost AS SELECT * FROM lynx.users WHERE vhostid IS NOT NULL PRIMARY KEY (vhostid, userlogin)");
-        
+
         CreateTableSpecification suggest_tagk = CreateTableSpecification.createTable("suggest_tagk")
                 .ifNotExists()
                 .partitionKeyColumn("vhostid", DataType.uuid())
                 .clusteredKeyColumn("tagk", DataType.text());
-        
+
         ct.execute(suggest_tagk);
-        
+
         CreateTableSpecification suggest_tagv = CreateTableSpecification.createTable("suggest_tagv")
                 .ifNotExists()
                 .partitionKeyColumn("vhostid", DataType.uuid())
                 .clusteredKeyColumn("tagv", DataType.text());
-        
+
         ct.execute(suggest_tagv);
-        
+
         CreateTableSpecification suggest_name = CreateTableSpecification.createTable("suggest_name")
                 .ifNotExists()
                 .partitionKeyColumn("vhostid", DataType.uuid())
                 .clusteredKeyColumn("name", DataType.text());
-        
+
         ct.execute(suggest_name);
-        
+
         CreateTableSpecification list = CreateTableSpecification.createTable("list")
                 .ifNotExists()
                 .partitionKeyColumn("vhostid", DataType.uuid())
                 .partitionKeyColumn("name", DataType.text())
                 .clusteredKeyColumn("group", DataType.cint())
                 .clusteredKeyColumn("tags", DataType.text());
-        
+
         ct.execute(list);
-        
+
         CreateTableSpecification series = CreateTableSpecification.createTable("series")
                 .ifNotExists()
                 .partitionKeyColumn("vhostid", DataType.uuid())
@@ -115,45 +112,45 @@ public class Setup {
                 .clusteredKeyColumn("time", DataType.cint())
                 .clusteredKeyColumn("random", DataType.smallint())
                 .column("value", DataType.cdouble());
-        
+
         ct.execute(series);
-        
+
         Insert systemVhost = QueryBuilder.insertInto("vhosts")
                 .value("vhostid", system.getId())
                 .value("vhostname", system.getName());
-        
+
         ct.execute(systemVhost);
 
         Insert adminUser = QueryBuilder.insertInto("users")
                 .value("userlogin", "admin")
-                .value("userpassword", BCrypt.hashpw("admin", BCrypt.gensalt(User.BCRYPT_ROUNDS)))
+                .value("userpassword", "2fa2849275b7af3e4137bcd867423462a409c1c264f7c5d8f67c3985429a6557")
                 .value("vhostid", system.getId())
                 .value("rank", User.Rank.RW_USER.name());
-        
+
         ct.execute(adminUser);
-        
+
         ct.getSession().getCluster().close();
-        
+
         System.exit(0);
-        
+
     }
-    
-    private static void init(){
-        
+
+    private static void init() {
+
         Cluster cluster = Cluster.builder()
                 .addContactPoint(System.getenv("cassandra_contactpoints"))
                 .build();
-        
+
         ct = new CassandraTemplate(cluster.connect());
     }
-    
-    private static void init(String _keyspace){
-        
+
+    private static void init(String _keyspace) {
+
         Cluster cluster = Cluster.builder()
                 .addContactPoint(System.getenv("cassandra_contactpoints"))
                 .build();
-        
+
         ct = new CassandraTemplate(cluster.connect(_keyspace));
     }
-    
+
 }
